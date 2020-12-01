7bbba86d6752457575886769b55351ce40c00655
griffin mbp <griffin.reed3@gmail.com>
2020-12-01 19:24:28 +0100

    Day 01

---
+++ day01/day01.py
@@ -0,0 +1,18 @@
+inpFile = open('numbers.txt', 'r')
+lines = inpFile.readlines()
+lines = sorted(lines)
+found = False
+for i in lines:
+	i = int(i)
+	for j in lines:
+		j = int(j)
+		for aids in lines:
+			aids = int(aids)
+			if i + j + aids == 2020:
+				print(i * j * aids)
+				found = True
+				break
+		if found:
+			break
+	if found:
+		break
